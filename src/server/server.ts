d2aa1a469ecaf0708e856f855377f08e25517a8f
Trocoli <ygoraraujoca@outlook.com>
2023-10-27 15:10:05 -0300

    server changes for multiple clients

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,14 +1,22 @@
 import { WebSocketServer } from "ws";
+import http from "http";
+import express from "express";
 
 const port = 1234;
-const wss = new WebSocketServer({ port });
 
-wss.on('connection', (ws) => {
-    ws.on('message', (data) => {
-        console.log(`Received message form client: ${data}`)
-    })
-    
-    ws.send(`Hello this is the websocket server`)
-})
+const server = http.createServer(express);
+const wss = new WebSocketServer({ server });
 
-console.log(`listening on port ${port}`)
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
+
+server.listen(port, function () {
+  console.log(`Server listening on port: ${port}`);
+});
